86e7dd05afc62809984c1eb97a7236b5ae3fc23b
Sriharshinid <sriharshinid@ucla.edu>
2020-07-12 01:04:51 -0700

    try dbloader

--- backend/src/main/java/com/bymeforme/backend/DatabaseLoader.java
+++ backend/src/main/java/com/bymeforme/backend/DatabaseLoader.java
@@ -1,7 +1,11 @@
 package com.bymeforme.backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
@@ -14,11 +18,14 @@ import java.io.IOException;
 public class DatabaseLoader implements CommandLineRunner{
     private final MyImageRepository repository;
 
+    @Value("classpath:static/api/food/*")
+    private Resource[] resources;
+
     @Autowired
     public DatabaseLoader(MyImageRepository repository) {
         this.repository = repository;
     }
-
+/*
     public void loadImages(String folderName) throws IOException{
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         File file = new File(classLoader.getResource(folderName).getFile());
@@ -28,9 +35,32 @@ public class DatabaseLoader implements CommandLineRunner{
         }
     }
 
+
+    public void loadImages(String folderName) throws IOException{
+    	for (final Resource res : resources) {
+            String name = res.getFilename();
+            this.repository.save(new MyImage(name, "food/" + name, name.substring(0, name.indexOf("."))));
+	    }
+    }
+*/
+
+    public void loadImages(String folderName) throws IOException{
+        ClassLoader cl = this.getClass().getClassLoader();
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(cl);
+        try {
+            Resource resources[] = resolver.getResources("classpath:"+ folderName +"/**");
+            for (final Resource res : resources) {
+                String name = res.getFilename();
+                this.repository.save(new MyImage(name, "food/" + name, name.substring(0, name.indexOf("."))));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
 	public void run(String... strings) throws Exception {
-        // loadImages("static/api/food");
-        this.repository.save(new MyImage("TEST", "food/TEST", "THIS IS A TEST"));
-	}
+        loadImages("static/api/food");
+	    //this.repository.save(new MyImage("TEST", "food/TEST", "THIS IS A TEST"));
+    }
+    
 }
