2429642763bf7c10bee40d06d349dbe6f1851ac8
Sriharshinid <sriharshinid@ucla.edu>
2020-07-14 13:37:56 -0700

    home pg pic

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,16 +5,16 @@ class Home extends React.Component {
   render() {
     return (
         <div>
-            <h1>hElLo wOrlD</h1>
+            <h1>hElLo w<span><img src={Spongebob} style={{height: "3%", width: "3%", objectFit: "contain", borderRadius: "80%"}}></img></span>rlD</h1>
             <p>Welcome to a site of random things made by me.</p>
-            <div style={{
+            {/* <div style={{
                 display: "block",
                 margin: "auto",
                 minHeight: "1px",
                 width: "20%",
                 overflow: "auto"}}>
               <img src={Spongebob} style={{height: "100%", width: "100%", objectFit: "contain"}}></img>
-            </div> 
+            </div>  */}
         </div>
     );
   }
